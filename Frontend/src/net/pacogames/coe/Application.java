c0e52ef3be2f1dbe5caae14dbd835362ec642ac0
Semillita <semillita.business@gmail.com>
2021-10-25 00:41:13 +0200

    Improved resource file tree and basic Attack class

--- Frontend/src/net/pacogames/coe/Application.java
+++ Frontend/src/net/pacogames/coe/Application.java
@@ -18,10 +18,11 @@ public class Application extends ApplicationAdapter {
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		scene = new MainMenu();
+		scene = new Game();
+		
 		//Borderless windowed fullscreen
-		//System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
-	    //Gdx.graphics.setWindowedMode(Gdx.graphics.getDisplayMode().width, Gdx.graphics.getDisplayMode().height);
+		System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
+	    Gdx.graphics.setWindowedMode(Gdx.graphics.getDisplayMode().width, Gdx.graphics.getDisplayMode().height);
 	    
 	    nanoTime = System.nanoTime();
 	}
