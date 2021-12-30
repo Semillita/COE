2281e5d9c213f5387f0f3aead51cd76751406912
Semillita <semillita.business@gmail.com>
2021-12-30 01:21:59 +0100

    Code cleanup

--- Frontend/src/net/pacogames/coe/game/GameGraphicsImpl.java
+++ Frontend/src/net/pacogames/coe/game/GameGraphicsImpl.java
@@ -13,28 +13,29 @@ public class GameGraphicsImpl implements GameGraphics {
 
 	private static final int VIEWPORT_WIDTH = 3840;
 	private static final int VIEWPORT_HEIGHT = 2160;
-	
+
 	private Camera camera;
 	private Viewport viewport;
-	
+
 	private PlayerSprite player1, player2;
-	
+
 	public GameGraphicsImpl() {
 		initCameraViewport();
-		
+
 		player1 = new PlayerSprite(1);
 		player2 = new PlayerSprite(2);
 	}
-	
+
 	@Override
 	public void renderFrame(Batch batch, Frame frame) {
 		batch.setProjectionMatrix(camera.combined);
-		
+
 		var p1pos = frame.player1data.pos;
 		var p2pos = frame.player2data.pos;
-		
+
 		player1.render(batch, p1pos.x, p1pos.y);
 		player2.render(batch, p2pos.x, p2pos.y);
+
 	}
 
 	@Override
@@ -49,5 +50,5 @@ public class GameGraphicsImpl implements GameGraphics {
 		camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
 		viewport = new ExtendViewport(3840, 2160, camera);
 	}
-	
+
 }
--- Frontend/src/net/pacogames/coe/menu/MainMenu.java
+++ Frontend/src/net/pacogames/coe/menu/MainMenu.java
@@ -19,28 +19,28 @@ import net.pacogames.coe.ui.buttons.Button;
 import net.pacogames.coe.ui.buttons.PlayButton;
 
 public class MainMenu implements Scene {
-	
+
 	private final Runnable onPlay;
-	
+
 	private Camera camera;
 	private Viewport viewport;
 	private MenuPage page;
 	private NavBar navBar;
 	private Texture bg;
-	
+
 	private final int VIEWPORT_WIDTH = 3840, VIEWPORT_HEIGHT = 2160;
-	
+
 	public MainMenu(Runnable onPlay) {
 		this.onPlay = onPlay;
 		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
 		viewport = new ExtendViewport(3840, 2160, camera);
-		navBar = new NavBar((int) (VIEWPORT_WIDTH - viewport.getWorldWidth())/2, (int) viewport.getWorldHeight() - 200, 
-				(int) viewport.getWorldWidth(), 200, (property) -> {
-					switch(property) {
+		navBar = new NavBar((int) (VIEWPORT_WIDTH - viewport.getWorldWidth()) / 2,
+				(int) viewport.getWorldHeight() - 200, (int) viewport.getWorldWidth(), 200, (property) -> {
+					switch (property) {
 					case PLAY:
 						setPage(new PlayPage((prop) -> {
-							if(prop == PlayButton.Property.NORMAL) {
+							if (prop == PlayButton.Property.NORMAL) {
 								onPlay.run();
 							}
 						}));
@@ -50,38 +50,43 @@ public class MainMenu implements Scene {
 					}
 				});
 		setInputListener();
-		
+
 		page = new PlayPage((prop) -> {
-			if(prop == PlayButton.Property.NORMAL) {
+			if (prop == PlayButton.Property.NORMAL) {
 				onPlay.run();
 			}
 		});
 		bg = Resources.getTexture("backgrounds/main_menu.png");
 	}
-	
+
 	@Override
 	public void render(Batch batch) {
 		batch.setProjectionMatrix(camera.combined);
-		batch.draw(bg, (viewport.getWorldWidth() - VIEWPORT_WIDTH)/2, (viewport.getWorldHeight() - VIEWPORT_HEIGHT)/2, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+		batch.draw(bg, (viewport.getWorldWidth() - VIEWPORT_WIDTH) / 2,
+				(viewport.getWorldHeight() - VIEWPORT_HEIGHT) / 2, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 		navBar.render(batch);
 		page.render(batch);
-		page.resize((int) (viewport.getWorldWidth() - VIEWPORT_WIDTH)/2, (int) viewport.getWorldWidth(), (int) viewport.getWorldHeight() - 200);
+		page.resize((int) (viewport.getWorldWidth() - VIEWPORT_WIDTH) / 2, (int) viewport.getWorldWidth(),
+				(int) viewport.getWorldHeight() - 200);
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		viewport.update(width, height, true);
 		camera.position.set(VIEWPORT_WIDTH / 2f, VIEWPORT_HEIGHT / 2f, 0);
-		viewport.apply();		
-		
-		navBar.setBounds((int) (VIEWPORT_WIDTH - viewport.getWorldWidth())/2, (int) (viewport.getWorldHeight() - 200 - (viewport.getWorldHeight() - VIEWPORT_HEIGHT)/2), 
+		viewport.apply();
+
+		navBar.setBounds((int) (VIEWPORT_WIDTH - viewport.getWorldWidth()) / 2,
+				(int) (viewport.getWorldHeight() - 200 - (viewport.getWorldHeight() - VIEWPORT_HEIGHT) / 2),
 				(int) viewport.getWorldWidth(), 200);
-		
-		page.resize((int) (viewport.getWorldWidth() - VIEWPORT_WIDTH)/2, (int) viewport.getWorldWidth(), (int) viewport.getWorldHeight() - 200);
+
+		page.resize((int) (viewport.getWorldWidth() - VIEWPORT_WIDTH) / 2, (int) viewport.getWorldWidth(),
+				(int) viewport.getWorldHeight() - 200);
 	}
+
 	public void setInputListener() {
 		Gdx.input.setInputProcessor(new InputAdapter() {
-			
+
 			@Override
 			public boolean mouseMoved(int x, int y) {
 				Vector3 worldCords = new Vector3(x, y, 0);
@@ -114,17 +119,17 @@ public class MainMenu implements Scene {
 				page.mouseReleased(x, y);
 				return false;
 			}
-			
+
 			@Override
 			public boolean touchDragged(int x, int y, int arg2) {
 				mouseMoved(x, y);
 				return false;
 			}
-		}); 
+		});
 	}
-	
+
 	private void setPage(MenuPage page) {
-		if(page.getClass() != this.page.getClass()) {
+		if (page.getClass() != this.page.getClass()) {
 			this.page = page;
 		}
 	}
